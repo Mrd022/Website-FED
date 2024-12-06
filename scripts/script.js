0411d19b01a43d834e681b555570c01a9694c375
Mrd022 <145541551+Mrd022@users.noreply.github.com>
2024-12-06 16:16:37 +0100

    Juiste versie

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,15 @@
-// JavaScript Document
-console.log("hi");
+var deButton = document.querySelector("header ul button");
+var deCloseButton = document.querySelector("header nav button");
+
+deButton.onclick = addMenu;
+deCloseButton.onclick = removeMenu;
+
+function addMenu(){
+    var deNav = document.querySelector("nav");
+    deNav.classList.add("toonMenu");
+}
+
+function removeMenu(){
+    var deNav = document.querySelector("nav");
+    deNav.classList.remove("toonMenu");
+}
